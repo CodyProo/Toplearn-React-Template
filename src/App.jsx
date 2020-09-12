98df29229f78b384914d333daf4ec45074102711
Eddie <cody18v@gmail.com>
2020-09-12 08:15:27 -0700

    add latest animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import Home from './views/Home';
 import ArchiveCourses from './views/ArchiveCourses';
 import SingleCourse from './views/SingleCourse';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const App = () => {
 	return (
 		<Router>
-			<Switch>
-				<Route path='/' exact component={Home} />
-				<Route path={'/archive/courses'} component={ArchiveCourses} />
-				<Route path={'/single/course/:id'} component={SingleCourse} />
-			</Switch>
+			<Route
+				render={({ location }) => (
+					<TransitionGroup>
+						<CSSTransition key={location.key} timeout={2000} classNames='fade'>
+							<Switch location={location}>
+								<Route path='/' exact component={Home} />
+								<Route path={'/archive/courses'} component={ArchiveCourses} />
+								<Route path={'/single/course/:id'} component={SingleCourse} />
+							</Switch>
+						</CSSTransition>
+					</TransitionGroup>
+				)}></Route>
 		</Router>
 	);
 };
